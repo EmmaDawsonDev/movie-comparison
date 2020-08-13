42b23c480757125ecd7a8c45dec1fd45410344ad
Emma Dawson <emma.l.dawson@gmail.com>
2020-08-13 12:20:37 +0200

    Add second API call to get movie data and display on screen

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const fetchData = async (searchTerm) => {
     return response.data.Search;
 };
 
+
+//creating html dropdown menu with search results
 const root = document.querySelector(".autocomplete");
 root.innerHTML = `
 <label><b>Search for a movie</b></label>
@@ -39,8 +41,6 @@ const onInput = async (event) => {
         return;
     }
 
-
-
     resultsWrapper.innerHTML = "";
     dropdown.classList.add("is-active");
 
@@ -53,6 +53,12 @@ const onInput = async (event) => {
         <img src="${imgSrc}" />
         ${movie.Title}
         `;
+        option.addEventListener("click", () => {
+            dropdown.classList.remove("is-active");
+            input.value = movie.Title;
+            onMovieSelect(movie);
+        });
+
 
         resultsWrapper.appendChild(option);
     }
@@ -62,8 +68,62 @@ const onInput = async (event) => {
 
 input.addEventListener("input", debounce(onInput));
 
+
+//removing the dropdown menu if the user clicks anywhere else but the menu.
 document.addEventListener("click", event => {
     if (!root.contains(event.target)) {
         dropdown.classList.remove("is-active");
     }
 });
+
+//Making a second API request for specific movie data
+const onMovieSelect = async movie => {
+    const response = await axios.get("http://www.omdbapi.com/", {
+        params: {
+            apikey: ,
+            i: movie.imdbID
+        }
+    });
+
+    document.querySelector("#summary").innerHTML = movieTemplate(response.data);
+}
+
+//Creating the html template for the movie information
+const movieTemplate = (movieDetail) => {
+    return `
+    <article class="media">
+  <figure class="media-left">
+    <p class="image">
+      <img src="${movieDetail.Poster}" alt="">
+    </p>
+  </figure>
+  <div class="media-content">
+    <div class="content">
+      <h1>${movieDetail.Title}</h1>
+      <h4>${movieDetail.Genre}</h4>
+      <p>${movieDetail.Plot}</p>
+    </div>
+  </div>
+</article>
+<article class="notification is-primary">
+  <p class="title">${movieDetail.Awards}</p>
+  <p class="subtitle">Awards</p>
+</article>
+<article class="notification is-primary">
+  <p class="title">${movieDetail.BoxOffice}</p>
+  <p class="subtitle">Box Office</p>
+</article>
+<article class="notification is-primary">
+  <p class="title">${movieDetail.Metascore}</p>
+  <p class="subtitle">Metascore</p>
+</article>
+<article class="notification is-primary">
+  <p class="title">${movieDetail.imdbRating}</p>
+  <p class="subtitle">IMDB Rating</p>
+</article>
+<article class="notification is-primary">
+  <p class="title">${movieDetail.imdbVotes}</p>
+  <p class="subtitle">IMDB Votes</p>
+</article>
+    `;
+};
