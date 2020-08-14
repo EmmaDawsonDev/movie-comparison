4ae51772246173aa86c988981723126eb3e802a4
Emma Dawson <emma.l.dawson@gmail.com>
2020-08-14 22:01:06 +0200

    change made to styling

--- index.js
+++ index.js
@@ -87,10 +87,10 @@ const leftSideValue = parseInt(leftStat.dataset.value);
 const rightSideValue = parseInt(rightStat.dataset.value);
 
 if (rightSideValue > leftSideValue) {
-  leftStat.classList.remove("is-primary");
+  leftStat.classList.remove("is-success");
   leftStat.classList.add("is-danger");
 } else {
-  rightStat.classList.remove("is-primary");
+  rightStat.classList.remove("is-success");
   rightStat.classList.add("is-danger");
 }
 
@@ -134,23 +134,23 @@ const awards = movieDetail.Awards.split(" ").reduce((prev, word) => {
     </div>
   </div>
 </article>
-<article data-value=${awards} class="notification is-primary">
+<article data-value=${awards} class="notification is-success">
   <p class="title">${movieDetail.Awards}</p>
   <p class="subtitle">Awards</p>
 </article>
-<article data-value=${dollars} class="notification is-primary">
+<article data-value=${dollars} class="notification is-success">
   <p class="title">${movieDetail.BoxOffice}</p>
   <p class="subtitle">Box Office</p>
 </article>
-<article data-value=${metascore} class="notification is-primary">
+<article data-value=${metascore} class="notification is-success">
   <p class="title">${movieDetail.Metascore}</p>
   <p class="subtitle">Metascore</p>
 </article>
-<article data-value=${imdbRating} class="notification is-primary">
+<article data-value=${imdbRating} class="notification is-success">
   <p class="title">${movieDetail.imdbRating}</p>
   <p class="subtitle">IMDB Rating</p>
 </article>
-<article data-value=${imdbVotes} class="notification is-primary">
+<article data-value=${imdbVotes} class="notification is-success">
   <p class="title">${movieDetail.imdbVotes}</p>
   <p class="subtitle">IMDB Votes</p>
 </article>
